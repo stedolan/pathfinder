be1e75ff16c524fbd081855533270e0edc8ab4bc
Stephen Dolan <mu@netsoc.tcd.ie>
2012-06-08 13:47:57 +0100

    Proper packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="Pathfinder",
+    author="Stephen Dolan",
+    author_email="mu@netsoc.tcd.ie",
+    url="https://github.com/stedolan/pathfinder",
+    version="0.1",
+    packages=["pathfinder"],
+    license="MIT license",
+    long_description=open("README.txt").read())
+    
